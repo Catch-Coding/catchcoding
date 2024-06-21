0fe751d14ff3eb13a5484a94202a8f7216c5f2af
h3ldll <167717217+gvalue-04@users.noreply.github.com>
2024-06-21 16:26:21 -0400

    Create catch-uploader.js

---
+++ catch/vm/catch-uploader.js
@@ -0,0 +1,20 @@
+/*
+catch-uploader.js 
+written by H3L
+LICENSED UNDER GPL
+*/
+function openProject {
+  const fileInput = document.getElementById('fileInput');
+  fileInput.click();
+  fileInput.onchange = () => {
+    const file = fileInput.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        const content = e.target.result;
+        document.getElementById('codeArea').value = content;
+      }
+      reader.readAsText(file;
+    }
+  };
+}
